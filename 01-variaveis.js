426d49560408d3c45752d716d237507800a9aab0
Ana clara lopes <analopes3489@gmail.com>
2024-02-07 11:26:34 -0300

    javascript- basico

---
+++ 01-variaveis.js
@@ -0,0 +1,10 @@
+// Criando variáveis com "var" (var nome variável = <valor>;)
+
+var nome = "Ana clara"; //var cria variável global
+var idade = 16;
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log("agora fiz aniversário! Tenho", idade+1);
+
+//aumentando automagicamente + 1 (autoincremento);
+idade++ //idade = idade+1
+console.log("Meu nome é", nome, "e tenho", idade, "anos!")
---
+++ 02-aritimetico.js
@@ -0,0 +1,25 @@
+//utilizando computadores aritmetricos com JavaScript
+
+//console.log("a soma entre 8 e 7 é:",8+7);
+
+//efetuar a soma de dois numeros informados por você (var nome variavel = 12)
+
+var x = 6;
+var y = 6;
+console.log("a soma entre", x, "e", y, "é:", x + y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("multiplicacao =", multiplicacao);
+console.log("divisao =", divisao);
+console.log("subtrasao =", subtracao);
+console.log("Soma =", soma);
+//regras para nomear identificadores (var,const ou let)
+//Podem começar com A-Z, a-z, _ ou $
+var Minhavariavel = 10;
+var outravariavel = 20;
+const _minhaConstrante = 3.1514;
+var $minhavariavel = 11;
