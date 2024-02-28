b0d327e64d28ac1ed562a3f351e2d120907e1d8a
Ana clara lopes <analopes3489@gmail.com>
2024-02-28 10:32:04 -0300

    atividade05-função

--- atividade5-ana.lopes.js
+++ atividade5-ana.lopes.js
@@ -4,7 +4,7 @@ function idade(ano1) {
 console.log(`A pessoa tem a idade: ${idade(2007)}`);
 
 //exercício dois
-function combustivel(litros, km) {
+function quantidade(litros, km) {
   return litros * km;
 }
-console.log(`Ele pode viajar com 40 litros de combustível : ${combustivel(40, 12)} kms`);
+console.log(`Ele pode viajar com 40 litros de combustível : ${quantidade(40, 12)} kms`);
