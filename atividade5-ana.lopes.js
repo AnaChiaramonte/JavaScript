651b80de811cb329cc8d31231d3a9fd250d52301
Ana clara lopes <analopes3489@gmail.com>
2024-03-05 07:46:52 -0300

    atividade-05

--- atividade5-ana.lopes.js
+++ atividade5-ana.lopes.js
@@ -1,15 +1,15 @@
-// function idade(ano1) {
-//   return 2024 - ano1;
-// }
-// console.log(`A pessoa tem a idade: ${idade(2007)}`);
+function idade(ano1) {
+  return 2024 - ano1;
+}
+console.log(`A pessoa tem a idade: ${idade(2007)}`);
 
-// //exercício dois
-// function quantidade(litros, km) {
-//   return litros * km;
-// }
-// console.log(
-//   `Ele pode viajar com 40 litros de combustível : ${quantidade(40, 12)} kms`
-// );
+//exercício dois
+function quantidade(litros, km) {
+  return litros * km;
+}
+console.log(
+  `Ele pode viajar com 40 litros de combustível : ${quantidade(40, 12)} kms`
+);
 
 // console.log("---------------------------------atividade 2---------------------------------------" );
 
